cde30ca265afddd1e40d0029f690d8ef086887c7
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-04-19 20:04:53 -0400

    created a basic layout with the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl">Welcome to Youtube clone.</div>;
+  return (
+    <div>
+      {/*
+    Header
+    Body
+      Sidebar
+        MenuItems
+      MainContainer
+        ButtonsList
+        VideoContainer
+          VideoCard
+    */}
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
