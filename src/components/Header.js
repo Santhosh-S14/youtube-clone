1ff6a78ce4c280669041868ef393cce284b24573
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-04-20 11:00:57 -0400

    feature: added different icons for the Header and aligned them

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
 import React from "react";
-import { Menu } from "lucide-react";
+import { Bell, Menu, Mic, Video } from "lucide-react";
 import { Avatar } from "@mui/material";
 
 const Header = () => {
   return (
-    <div className="grid grid-flow-col p-1">
-      <div className="col-span-1 flex">
-        <Menu size={30} className="h-8" />
+    <div className="grid grid-flow-col px-4 py-2 shadow-lg">
+      <div className="col-span-1 flex items-center">
+        <Menu size={24} className="text-gray-700 cursor-pointer" />
         <img
-          className="w-30 mx-2 h-8"
+          className="w-30 mx-2 h-6"
           src="https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg"
           alt="youtube logo"
         ></img>
       </div>
-      <div className="col-span-10 text-center px-10">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-1/2 border border-gray-400 rounded-l-full p-2"
-        ></input>
-        <button className="border border-gray-400 rounded-r-full px-5 py-2">
-          Search
-        </button>
+      <div className="col-span-6">
+        <div className="flex justify-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-1/2 border border-gray-300 rounded-l-full px-4 py-2 focus:outline-none"
+          ></input>
+          <button className="border border-gray-400 rounded-r-full px-5 py-2">
+            Search
+          </button>
+          <button className="bg-gray-100 border border-gray-300 rounded-full px-2 py-2 ml-2">
+            <Mic size={24} className="text-gray-700" />
+          </button>
+        </div>
       </div>
-      <div className="col-span-1">
-        <Avatar className="">S</Avatar>
+      <div className="col-span-3 flex justify-end items-center space-x-6">
+        <Video size={24} className="text-gray-700 cursor-pointer" />
+        <Bell size={24} className="text-gray-700 cursor-pointer" />
+        <div className="relative">
+          <Avatar className="cursor-pointer" />
+        </div>
       </div>
     </div>
   );
