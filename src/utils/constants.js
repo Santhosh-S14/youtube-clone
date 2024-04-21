16be1f02816e6f4d9b6dd39934d163c8e1667ac0
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-04-21 02:10:44 -0400

    fix: updated api key for security

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDLIs4EJyuIB7H1C0YbpItMBA89fKBW948";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics&chart=mostPopular&maxResults=50&regionCode=CA&key=" +
